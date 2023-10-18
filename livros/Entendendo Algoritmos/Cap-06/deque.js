44f7c7fa1959d4b171aab09bfa46ccaa1e9ac7d9
Vitor Norton <vtnorton@outlook.com>
2023-10-18 19:49:48 -0300

    grafos

---
+++ livros/Entendendo Algoritmos/Cap-06/deque.js
@@ -0,0 +1,35 @@
+function FilaDePesquisa() {
+  let grafo = {};
+
+  grafo["voce"] = ["alice", "bob", "claire"];
+  grafo["bob"] = ["anuj", "peggy"];
+  grafo["alice"] = ["peggy"];
+  grafo["claire"] = ["thom", "jonny"];
+  grafo["anuj"] = [];
+  grafo["peggy"] = [];
+  grafo["thom"] = [];
+  grafo["jonny"] = [];
+
+  let fila_de_pesquisa = []; //push, shift
+  let visitados = [];
+  fila_de_pesquisa.push(...grafo["voce"]);
+
+  for (let i = 0; i < fila_de_pesquisa.length; i++) {
+    let pessoa = fila_de_pesquisa[i];
+    if (!visitados.includes(pessoa)) {
+      visitados.push(pessoa);
+      if (pessoa_eh_vendedor(pessoa)) {
+        console.log(pessoa + " é um vendedor de manga!");
+        return true;
+      } else {
+        fila_de_pesquisa.push(...grafo[pessoa]);
+      }
+    }
+  }
+}
+
+function pessoa_eh_vendedor(pessoa) {
+  return pessoa === "thom";
+}
+
+FilaDePesquisa();
---
+++ livros/Entendendo Algoritmos/Cap-06/deque.py
@@ -0,0 +1,13 @@
+grafo = {}
+grafo["voce"] = ["alice", "bob", "claire"]
+grafo["bob"] = ["anuj", "peggy"]
+grafo["alice"] = ["peggy"]
+grafo["claire"] = ["thom", "jonny"]
+grafo["anuj"] = []
+grafo["peggy"] = []
+grafo["thom"] = []
+grafo["jonny"] = []
+
+from collections import deque
+fila_de_pesquisa = deque()
+fila_de_pesquisa += grafo["voce"]
